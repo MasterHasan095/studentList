a20cc58031a5a890c2a38340dd599c02f29b80b0
Master Hasan <123147147+MasterHasan095@users.noreply.github.com>
2023-05-25 18:17:58 -0400

    Update May25.java
    Change In GitHub

--- src/may25/May25.java
+++ src/may25/May25.java
@@ -15,12 +15,15 @@ public class May25 {
      */
     
     private String name;
-    private String studentID;
+    private Integer studentID;
     private String address;
     
-    public May25(String givenName){
+    public May25(String givenName, Integer givenStudentId){
         this.name = givenName;
+        this.studentID = givenStudentId;
     }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
